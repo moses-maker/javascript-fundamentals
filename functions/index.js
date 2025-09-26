6a412bb6d3893fbc9819356b90ffbc2f39dd496d
moses adala <67926121+moses-maker@users.noreply.github.com>
2025-09-26 14:50:38 +0300

    Add files via upload

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+// take user inputsusing prompt
+let math = Number(prompt("Enter marks for maths:"));
+let eng = Number(prompt("Enter marks for english:"));
+let kis = Number(prompt("Enter marks for kiswahili:"));
+
+// calculate the totl marks
+let total_marks = math + eng + kis;
+
+document.writeln(total_marks);
+
